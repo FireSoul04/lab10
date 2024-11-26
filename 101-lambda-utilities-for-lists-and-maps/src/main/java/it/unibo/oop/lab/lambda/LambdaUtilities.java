9f889119d06b3d1aaf920bac23e5548735d6fffe
Davide Mancini <davide.mancini10@studio.unibo.it>
2024-11-26 16:33:52 +0100

    Done ex 101 of oop in lab

--- 101-lambda-utilities-for-lists-and-maps/src/main/java/it/unibo/oop/lab/lambda/LambdaUtilities.java
+++ 101-lambda-utilities-for-lists-and-maps/src/main/java/it/unibo/oop/lab/lambda/LambdaUtilities.java
@@ -2,6 +2,7 @@ package it.unibo.oop.lab.lambda;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -61,7 +62,9 @@ public final class LambdaUtilities {
         /*
          * Suggestion: consider Optional.filter
          */
-        return null;
+        final List<Optional<T>> passedOpts = new ArrayList<>();
+        list.forEach(t -> passedOpts.add(Optional.of(t).filter(pre::test)));
+        return passedOpts;
     }
 
     /**
@@ -80,7 +83,13 @@ public final class LambdaUtilities {
         /*
          * Suggestion: consider Map.merge
          */
-        return null;
+        final Map<R, Set<T>> passedElems = new HashMap<>();
+        list.forEach(t -> passedElems.merge(op.apply(t), Set.of(t), (x, y) -> {
+            final Set<T> z = new HashSet<>(x);
+            z.add(t);
+            return z;
+        }));
+        return passedElems;
     }
 
     /**
@@ -101,7 +110,9 @@ public final class LambdaUtilities {
          *
          * Keep in mind that a map can be iterated through its forEach method
          */
-        return null;
+        final Map<K, V> passedElems = new HashMap<>();
+        map.forEach((k, v) -> passedElems.put(k, v.orElse(def.get())));
+        return passedElems;
     }
 
     /**
